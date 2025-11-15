27eecd8ba7f1c3cbdd1e2eb5febe658975f0983f
mengkueh <152407358+mengkueh@users.noreply.github.com>
2025-11-16 00:59:26 +0800

    fix bug on vercel 5

--- src/app/api/notes/[id]/share/route.ts
+++ src/app/api/notes/[id]/share/route.ts
@@ -1,4 +1,5 @@
 // app/api/notes/[id]/share/route.ts
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { getSessionFromCookie } from "@/lib/auth";
